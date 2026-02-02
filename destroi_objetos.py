c1b8967e10954c01d3e5dbb10360049885029bcd
Valdir Sevaios <valdirnsjunior@gmail.com>
2026-02-02 00:41:41 -0300

    .

--- destroi_objetos.py
+++ destroi_objetos.py
@@ -6,7 +6,3 @@
 # DBTITLE 1,Drop schema
 print(f"Drop Schema {schema}")
 spark.sql(f"DROP SCHEMA IF EXISTS {schema} CASCADE")
-
-# COMMAND ----------
-print(f"Drop Volume {volume}")
-spark.sql(f"DROP VOLUME IF EXISTS {volume}")
